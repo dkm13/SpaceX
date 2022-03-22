3bab6afccdd4a16f8d2de2cebb01d4825bd841c7
Dibran <80331350+dkm13@users.noreply.github.com>
2022-03-22 19:35:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,15 +15,3 @@ function hideMenu() {
     backdrop[i].style.filter = "brightness(100%)";
   }
 }
-
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.opacity = "1";
-  } else {
-    document.getElementById("navbar").style.opacity = "0";
-  }
-
-  prevScrollpos = currentScrollPos;
-};
